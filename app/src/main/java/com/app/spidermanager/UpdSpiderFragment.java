3891a009485219f6bdd3bd0a853119cbced422c2
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2022-06-02 00:38:49 +0300

    Выбор картинки "типа" работает

--- app/src/main/java/com/app/spidermanager/UpdSpiderFragment.java
+++ app/src/main/java/com/app/spidermanager/UpdSpiderFragment.java
@@ -1,14 +1,30 @@
 package com.app.spidermanager;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import com.app.spidermanager.databinding.UpdSpiderFragmentBinding;
+
+import java.io.IOException;
 
 public class UpdSpiderFragment extends Fragment {
 
@@ -28,15 +44,50 @@ public class UpdSpiderFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonBack.setOnClickListener(view1 ->
+        binding.buttonBack.setOnClickListener(v ->
                 NavHostFragment.findNavController(UpdSpiderFragment.this)
                 .navigate(R.id.action_UpdSpiderFragment_to_SpidersFragment));
+
+        binding.updPhoto.setOnClickListener(v -> {
+            int permission = ContextCompat.checkSelfPermission(
+                    requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+            if(permission != PackageManager.PERMISSION_GRANTED)
+            {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                activityResultLauncher.launch(intent);
+            }
+            else{
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        2000);
+            }
+        });
     }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null){
+                        Uri selected = data.getData();
+                        try{
+                            Bitmap bitmap = MediaStore.Images.
+                                    Media.getBitmap(getActivity().getContentResolver(),
+                                    selected);
+                            binding.updPhoto.setImageBitmap(bitmap);
+                        }
+                        catch (IOException e){
+                            Log.i("DEBUG", "activityResultLauncher - IOException");
+                        }
+                    }
+                }
+            });
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
-
 }
